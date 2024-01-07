f76dc5305a9236bfa4c760b05221c1b8840e89d6
EmilKavara <kavaraemil@gmail.com>
2024-01-07 23:49:05 +0100

    Added accessories

---
+++ js/accessories.js
@@ -0,0 +1,9 @@
+function changePage(page,number){
+    if(page=="speaker"){
+        window.location.href="speaker.html";
+    }else if(page=="charger"){
+        window.location.href="charger.html";
+    }else if(page=="airpods"){
+        window.location.href="airpods.html";
+    }
+}
