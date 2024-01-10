0a2a80ffc3c3460c392db488d5c92da3ca64cc1d
Edin-code <edinmusanovic4@gmail.com>
2024-01-10 17:15:58 +0100

    Small changes in accessories

--- js/accessories.js
+++ js/accessories.js
@@ -5,5 +5,11 @@ function changePage(page,number){
         window.location.href="charger.html";
     }else if(page=="airpods"){
         window.location.href="airpods.html";
+    }else if(page=="iphone"){
+        window.location.href="iphone.html";
+    }else if(page=="xiaomi"){
+        window.location.href="xiaomi.html";
+    }else if(page=="samsung"){
+        window.location.href="samsung.html";
     }
 }
